a456c2607d65587c1c88b811085f114e4eb25b17
xuanlocle <xuanlocle2412@gmail.com>
2020-04-17 22:51:29 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,14 @@ import 'Diary_Home.dart';
 
 void main() => runApp(Diary());
 
+/**
+* Nên tạo State< extends từ StatefulWidget>
+**/
+
+
+/*
+* Nên đặt tên class theo chuẩn chung, có ý nghĩa, đừng tùy hứng. e.g: DiaryScreen, DiaryScreenState,...
+*/
 class Diary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,7 +25,9 @@ class Diary extends StatelessWidget {
 
         ),
         home: new DiaryHome(
-
+/**
+* Xóa khoảng trống vô nghĩa này nha. Nhớ format code cho đẹp (Ctrl Alt L) 
+**/
 
         ),
         debugShowCheckedModeBanner: false,
